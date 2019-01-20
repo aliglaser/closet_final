a983df66194292ea83249760586258e31d8809fc
Natalie Glaser <nurinuriprettynuri@gmail.com>
2019-01-19 21:24:27 -0800

    layout

--- readymade_oatmeal/closet/forms.py
+++ readymade_oatmeal/closet/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.shortcuts import get_object_or_404
+#from crispy_forms.helper import FormHelper
 from . import models
 
 
@@ -22,6 +23,24 @@ class ClothesForm(forms.ModelForm):
 			'photoid',
 		]
 
+
+
+
+ 
+ 
+class TestForm(forms.Form):
+    """
+    Form to upload the screenshot of a webpage
+    """
+ 
+    image_data = forms.CharField(widget=forms.HiddenInput(), required=False)
+ 
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_id = "agreement_form"
+        self.helper.form_method = 'post'
+ 
+        super(TestForm, self).__init__(*args, **kwargs)
 
 Clothes_Formset = forms.modelformset_factory(
 	models.Clothes, 
